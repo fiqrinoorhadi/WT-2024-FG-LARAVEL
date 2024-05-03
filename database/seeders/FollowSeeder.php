bd8d59ff690455f0295b3bffcd90b7f8bd771bf8
fiqrinoorhadi <44854409+fiqrinoorhadi@users.noreply.github.com>
2024-05-03 07:32:37 +0700

    sd

--- database/seeders/FollowSeeder.php
+++ database/seeders/FollowSeeder.php
@@ -8,15 +8,11 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class FollowSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         Follow::create([
             'follower_id'   => 1,
             'following_id'   => 2,
-            'is_accepted'   => true
         ]);
         Follow::create([
             'follower_id'   => 1,
